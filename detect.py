bcc22851f2b528d7b547e4159a285b3b5a3b193a
vivekemmadi <115087955+vivekemmadi@users.noreply.github.com>
2024-09-24 11:46:17 +0530

    Add files via upload

---
+++ detect.py
@@ -0,0 +1,67 @@
+from ultralytics import YOLO
+import cv2
+import cvzone
+import math
+import time
+
+#video_path = r"C:\Users\svina\Downloads\people.mp4"
+cap = cv2.VideoCapture(0)  # Changed to default camera index
+
+# Check if the webcam opened successfully
+if not cap.isOpened():
+    print("Error: Could not open the camera.")
+    exit()
+
+cap.set(3, 1280)  # Set width
+cap.set(4, 720)   # Set height
+
+model = YOLO("../Yolo-Weights/yolov8l.pt")
+
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"
+              ]
+
+prev_frame_time = 0
+new_frame_time = 0
+
+while True:
+    new_frame_time = time.time()
+    success, img = cap.read()
+    
+    if not success:
+        print("Failed to capture image")
+        continue  # Skip the rest of the loop if the frame was not captured correctly
+
+    results = model(img, stream=True)
+    for r in results:
+        boxes = r.boxes
+        for box in boxes:
+            # Bounding Box
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            w, h = x2 - x1, y2 - y1
+            cvzone.cornerRect(img, (x1, y1, w, h))
+            # Confidence
+            conf = math.ceil((box.conf[0] * 100)) / 100
+            # Class Name
+            cls = int(box.cls[0])
+            cvzone.putTextRect(img, f'{classNames[cls]} {conf}', (max(0, x1), max(35, y1)), scale=1, thickness=1)
+
+    fps = 1 / (new_frame_time - prev_frame_time)
+    prev_frame_time = new_frame_time
+    print(fps)
+
+    cv2.imshow("Image", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break  # Exit loop when 'q' is pressed
+
+cap.release()
+cv2.destroyAllWindows()
